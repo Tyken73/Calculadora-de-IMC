fdc1b4936a6f2a1bc35f3e3f870899f526e40175
Tyken73 <tiago73rocha@gmail.com>
2023-03-14 00:37:14 +0900

    Versão final

--- www/components/script.js
+++ www/components/script.js
@@ -1,5 +1,5 @@
 function CalcIMC(){
-    var nome, imc, massa, altura, resultado 
+    var nome, imc, massa, altura, faixa, resultado 
 
     nome = document.getElementById('nome').value
     massa = document.getElementById('massa').value
@@ -7,7 +7,26 @@ function CalcIMC(){
 
     imc = massa / (altura * altura)
 
+    if (imc < 18,5) {
+      faixa = "magreza"
+    }
+    if (imc > 18,5 && imc < 24,9) {
+      faixa = "Saudável"
+    }
+    if (imc > 25 && imc < 29,9) {
+      faixa = "Sobrepeso"
+    }
+    if (imc > 30 && imc < 34,9) {
+      faixa = "Obesidade"
+    }
+    if (imc > 35 && imc < 39,9) {
+      faixa = "Obesidade severa"
+    }
+    if (imc > 40) {
+      faixa = "Obesidade morbida"
+    }
+
     resultado = document.getElementById('resultado')
 
-    resultado.innerHTML = nome + ', seu IMC é ' + imc.toFixed(2)
+    resultado.innerHTML = nome + ', seu IMC é ' + imc.toFixed(2) + ' e sua faixa é ' + faixa
 }
